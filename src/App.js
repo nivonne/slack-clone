0d777dd8fe87088e049550e536c988d3b24e53b1
nivonne <nimera27@gmail.com>
2021-01-26 21:20:09 -0600

    Initial Project Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Nadia</h1>
+
+      {/*header */}
+      {/*sidebar */}
+      {/*react-router -> chat-screen */}
+    </div>
+  );
+}
+
+export default App;
